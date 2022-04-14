a7f6c188509d57fec2ef0031e094b5ba0957e311
marcio <viana_marcio@outlook.com>
2022-04-14 15:31:40 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val nomes : Array<String> = arrayOf("Luis", "Maria", "Rogerio", "Marcio", "Marcelo", "Marcos")
+//        println(nomes[0])
+        for (i in nomes.indices) {
+            println(nomes[i])
+        }
+}
